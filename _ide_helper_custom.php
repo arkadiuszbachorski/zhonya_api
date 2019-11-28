2c594ec3cda19c44cf6d268051f738bc90a907bd
Arkadiusz Bachorski <quez2223@gmail.com>
2019-11-28 15:07:03 +0100

    Initial commit

---
+++ _ide_helper_custom.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arkadiusz
+ * Date: 04.07.2019
+ * Time: 10:01
+ */
+
+namespace Illuminate\Http;
+
+/**
+ * @method bool|array|null validate(array $rules, ...$params) Validate the given request with the given rules.
+ * @method array           validated()                        Get the validated data from the request.
+ */
+class Request
+{
+}
