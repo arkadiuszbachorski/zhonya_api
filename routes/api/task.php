9766ff0b426a85f295203caed59f41098c4636c4
Arkadiusz Bachorski <quez2223@gmail.com>
2019-11-30 13:39:29 +0100

    Repair edit links

--- routes/api/task.php
+++ routes/api/task.php
@@ -2,7 +2,7 @@
 
 Route::get('', 'TaskController@index');
 Route::post('', 'TaskController@store');
-Route::get('{task}', 'TaskController@edit')->middleware('can:manage,task');
+Route::get('{task}/edit', 'TaskController@edit')->middleware('can:manage,task');
 Route::put('{task}', 'TaskController@update')->middleware('can:manage,task');
 Route::delete('{task}', 'TaskController@destroy')->middleware('can:manage,task');
 
