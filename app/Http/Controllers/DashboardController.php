41b4bf68e404f44ecbb410f35b11a757231ef8fa
Arkadiusz Bachorski <quez2223@gmail.com>
2020-02-21 12:24:22 +0100

    Append necessary data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,17 +15,25 @@ class DashboardController extends Controller
         $tags = $user->tags()->withCount('tasks')
             ->orderBy('tasks_count', 'DESC')
             ->limit(self::SCORE_LIMIT)
-            ->get();
+            ->get()
+            ->hideInEach('description');
 
         $tasks = $user->tasks()->latest('updated_at')
             ->limit(self::SCORE_LIMIT)
-            ->get();
+            ->get()
+            ->hideInEach('description');
 
         $attempts = $user->attempts()
+            ->with('task')
             ->latest('updated_at')
             ->active()
             ->limit(self::SCORE_LIMIT)
-            ->get();
+            ->get()
+            ->each(function ($attempt) {
+                $attempt->append('short_description', 'relative_time');
+                $attempt->addHidden('laravel_through_key', 'description');
+                $attempt->task->addHidden('description', 'updated_at');
+            });
 
         return compact("tags", "tasks", "attempts");
     }
