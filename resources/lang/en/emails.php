a4a5c5e20d51fe8f3e512c06d9243c79d58c98f5
Arkadiusz Bachorski <quez2223@gmail.com>
2020-03-02 20:49:13 +0100

    Publish mail components

---
+++ resources/lang/en/emails.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'verify' => [
+        'subject' => 'Verify your Zhonya account',
+        'thank' => 'Thank you for registering!',
+        'click' => 'Please click the button below to verify your email address.',
+        'button' => 'Verify Email Address',
+        'notCreated' => 'If you did not create an account, no further action is required.',
+    ],
+
+];
---
+++ resources/lang/pl/emails.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'verify' => [
+        'subject' => 'Zweryfikuj swoje konto Zhonya',
+        'thank' => 'Dziękujemy za rejestrację',
+        'click' => 'Kliknij poniższy przycisk by zweryfikować swój adres email',
+        'button' => 'Zweryfikuj adres email',
+        'notCreated' => 'Jeśli nie tworzyłeś konta nie musisz podejmować żadnych akcji',
+    ],
+];
