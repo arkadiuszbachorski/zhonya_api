3961a658ff3f3130edefcc4ea006f143f0118a9a
Arkadiusz Bachorski <quez2223@gmail.com>
2020-03-05 15:09:23 +0100

    Create Contact controller

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Notifications\Contact;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
+
+class ContactController extends Controller
+{
+    public function contact(Request $request)
+    {
+        $data = $request->validate([
+            'name' => ['string','required'],
+            'email'=> ['email','required'],
+            'content' => ['required'],
+        ]);
+
+        Notification::route('mail', 'quez2223@gmail.com')
+            ->notify(new Contact($data));
+    }
+}
