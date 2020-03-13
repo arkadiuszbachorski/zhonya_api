f84a54b5b07b69e3885acd6cda50b6cb0a806c01
Arkadiusz Bachorski <quez2223@gmail.com>
2020-03-13 10:28:14 +0100

    Create PasswordResetController and register routes

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -2,6 +2,7 @@
 
 Route::post('login', 'Auth\LoginController@login');
 Route::post('register', 'Auth\RegisterController@register');
+Route::post('reset', 'Auth\PasswordResetController@reset');
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::post('send', 'Auth\VerificationController@send');
