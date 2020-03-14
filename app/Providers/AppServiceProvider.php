0fb81c776f7305c4b034fb39003e4a3ccd81b7b3
Arkadiusz Bachorski <quez2223@gmail.com>
2020-03-14 22:51:32 +0100

    Create standardDeviation and quartiles Collection method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -35,6 +35,36 @@ class AppServiceProvider extends ServiceProvider
                 $model->addHidden(...$fields);
             });
         });
+
+        Collection::macro('standardDeviation', function($key = null, $average = null) {
+            $values = isset($key) ? $this->pluck($key) : $this;
+
+            $average = $average ?? $values->avg();
+
+            $variance = $values->reduce(function ($carry, $item) use ($average, $key) {
+                return $carry + pow($item - $average, 2);
+            }, 0) / $this->count();
+
+            return sqrt($variance);
+        });
+
+        Collection::macro('quartiles', function($key = null) {
+            $length = $this->count();
+
+            $chunked = $this->chunk(ceil($length / 2));
+
+            $firstHalf = $chunked->get(0)->values();
+            if ($length % 2 !== 0) {
+                $firstHalf->pop();
+            }
+            $secondHalf = $chunked->get(1)->values();
+
+            $q1 = $firstHalf->median($key);
+            $q2 = $this->median($key);
+            $q3 = $secondHalf->median($key);
+
+            return compact('q1', 'q2', 'q3');
+        });
     }
 
     /**
